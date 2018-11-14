20a0c8fb1293a8277e33c3187356024b4a58bf67
avalonCLR <clement.lerest@gmail.com>
2018-11-14 15:56:12 +0100

    update personne

--- src/entites/AdressePostale.java
+++ src/entites/AdressePostale.java
@@ -2,9 +2,9 @@ package entites;
 
 public class AdressePostale {
 
-	int numero = 0;
-	String adresse = "";
-	int codePostal = 0;
-	String ville = "";
+	public int numero = 0;
+	public String adresse = "";
+	public int codePostal = 0;
+	public String ville = "";
 	
 }
--- src/essais/TestAdressePostale.java
+++ src/essais/TestAdressePostale.java
@@ -1,11 +1,23 @@
 package essais;
 
+import entites.AdressePostale;
+
 public class TestAdressePostale {
 
 	public static void main(String[] args) {
 		
+		AdressePostale adr1 = new AdressePostale();
+		adr1.numero = 5;
+		adr1.adresse = "rue du test";				
+		adr1.codePostal = 55000;
+		adr1.ville = "Lalande";
 		
-		//change
+		AdressePostale adr2 = new AdressePostale();
+		adr2.numero = 2;
+		adr2.adresse = "rue test aussi";
+		adr2.codePostal = 12345;
+		adr2.ville = "Turing";
+		
 		
 	}
 
