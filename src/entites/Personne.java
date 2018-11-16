29ae5ca30898c7413fadc4619b9cacf2911d558c
avalonCLR <clement.lerest@gmail.com>
2018-11-16 14:44:45 +0100

    add toString(), getters and setters, to Personne

--- src/entites/Personne.java
+++ src/entites/Personne.java
@@ -5,18 +5,52 @@ public class Personne {
 	public String nom = "";
 	public String prenom = "";
 	public String adressePostale = "";
-	
 
-	public Personne(String nom, String prenom) {
+	public Personne(String nom, String prenom, String adresse) {
 		this.nom = nom;
+		this.prenom = prenom;
+		setAdressePostale(adresse);
+	}
+
+//	public Personne(String nom, String prenom, String adressePostale) {
+//		this.nom = nom;
+//		this.prenom = prenom;
+//		this.adressePostale = adressePostale;
+//	}
+
+	public String toString() {
+		return " Nom "+nom.substring(0,1).toUpperCase()+nom.substring(1).toLowerCase()+" Prenom "+prenom+adressePostale;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+
+	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
 
 
-	public Personne(String nom, String prenom, String adressePostale) {
-		this.nom = nom;
-		this.prenom = prenom;
+	public String getAdressePostale() {
+		return adressePostale;
+	}
+
+
+	public void setAdressePostale(String adressePostale) {
 		this.adressePostale = adressePostale;
 	}
+
+	
+	
 	
 }
--- src/essais/TestPersonne.java
+++ src/essais/TestPersonne.java
@@ -6,9 +6,12 @@ public class TestPersonne {
 
 	public static void main(String[] args) {
 		
-		Personne p1 = new Personne("Duruc", "Jean-Michel", "rue Alan Turing");
+		Personne p1 = new Personne("Duruc", "Jean-Michel","rue Alan Turing");
 
 		Personne p2 = new Personne("Gildas", "Philippe", "rue du Pantheon");
+		
+		System.out.println(p1.toString());
+		
 		
 	}
 
